da349d8617522e848a4491e8b2976ed51fc1abf9
Exzone011 <143631857+Exzone011@users.noreply.github.com>
2024-08-07 21:10:23 -0300

    botao

--- scripts.js
+++ scripts.js
@@ -14,3 +14,25 @@ function moveSlide(n) {
     const slideWidth = slides[0].clientWidth;
     document.querySelector('.carousel-slide').style.transform = `translateX(${-slideIndex * slideWidth}px)`;
 }
+
+    // Pegue o botão
+    var mybutton = document.getElementById("backToTopBtn");
+
+    // Quando o usuário rolar para baixo 20px do topo do documento, mostre o botão
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // Quando o usuário clicar no botão, role para o topo do documento
+    function topFunction() {
+        document.body.scrollTop = 0; // Para Safari
+        document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE e Opera
+    }
